c644ae1632898c8ef9daa77a84635405572090b5
PatrickFrome <igradolius@gmail.com>
2026-08-26 17:11:57 +0300

    test(w1): require PID1 proc and core boundaries in S2 receipt

--- controller/w1/s2_runtime_canary_receipt.py
+++ controller/w1/s2_runtime_canary_receipt.py
@@ -34,6 +34,8 @@ PASS_MARKERS = {
     "OLDROOT_DETACHED": "true",
     "WORKSPACE_RW": "true",
     "NETWORK_DEFAULT_DENY": "true",
+    "RLIMIT_CORE_ZERO": "true",
+    "PID1_ENVIRON_DENIED": "true",
     "CANONICAL": "false",
     "AUTHORITY_EFFECT": "false",
     "WORKER_ADMITTED": "false",
